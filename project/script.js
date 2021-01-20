3cc5fe61ec9a308dcbb70244bb0ec9793b173d59
Bao phung <b.giaphung@gmail.com>
2021-01-20 16:38:32 -0600

    implemented add loan button functionality

--- project/script.js
+++ project/script.js
@@ -213,6 +213,28 @@ btnTransfer.addEventListener('click', function (e) {
 
 /////////////////////////////////////////////////
 
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (
+    amount > 0 &&
+    currentAccount.movements.some(function (mov) {
+      return mov >= amount * 0.1;
+    })
+  ) {
+    //add movement
+    currentAccount.movements.push(amount);
+
+    //update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
+/////////////////////////////////////////////////
+
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
 
